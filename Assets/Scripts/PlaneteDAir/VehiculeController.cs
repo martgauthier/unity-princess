29e5f9914b9edfe426ceb144557e7b2f6f6c0901
Gholab <hajarelgholabzouri@gmail.com>
2025-04-10 15:37:23 +0200

    rotation x et y bloquee

--- Assets/Scripts/PlaneteDAir/VehiculeController.cs
+++ Assets/Scripts/PlaneteDAir/VehiculeController.cs
@@ -31,27 +31,15 @@ public class VehiculeController : MonoBehaviour {
         float speed = inputY > 0 ? -forwardMoveSpeed : -backwardMoveSpeed; // inverted axis
         if (inputY == 0) speed = 0;
         rg.AddForce(this.transform.forward * speed, ForceMode.Acceleration);
-        // Steer
-        float rotation = inputX * steerSpeed * Time.fixedDeltaTime;
-        transform.Rotate(0, rotation, 0, Space.World);
-        
-       //// Adhérence au sol (anti flottement sur bosses ou descentes)
-       //RaycastHit hit;
-       //if (Physics.Raycast(transform.position, Vector3.down, out hit, 2f))
-       //{
-       //    rg.AddForce(-Vector3.up * 20f); // colle légèrement au sol
-       //}
-       //// Stabilisation douce pour éviter les basculements excessifs
-       //Vector3 velocityLocal = transform.InverseTransformDirection(rg.velocity);
-       //velocityLocal.y = 0;
-       //rg.velocity = transform.TransformDirection(velocityLocal);
 
-// Appliquer une force anti-basculement
-        //Vector3 predictedUp = Quaternion.AngleAxis(
-        //    rg.angularVelocity.magnitude * Mathf.Rad2Deg * Time.fixedDeltaTime, rg.angularVelocity) * transform.up;
-//
-        //Vector3 torqueVector = Vector3.Cross(predictedUp, Vector3.up);
-        //rg.AddTorque(torqueVector * 100f); // ajuste ce facteur pour plus ou moins de stabilité
+		// Steer (rotation uniquement sur Y, physiquement)
+        float rotation = inputX * steerSpeed * Time.fixedDeltaTime;
+        Quaternion turnOffset = Quaternion.Euler(0f, rotation, 0f);
+        rg.MoveRotation(rg.rotation * turnOffset);
+
+        // Stabilisation pour rester à plat (empêche les inclinaisons sur X/Z)
+        Vector3 euler = rg.rotation.eulerAngles;
+        rg.MoveRotation(Quaternion.Euler(0f, euler.y, 0f));
 
     }
     
